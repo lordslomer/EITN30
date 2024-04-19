1d99edd341e6bafc4fdd87058f2c54ec893b7e7c
SalamSki <slome.lord123@gmail.com>
2024-04-19 21:08:33 +0200

    delete myG each run

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 if [ $1 = "base" ]
 then
+  sudo ip link delete myG
   sudo iptables -F
   sudo iptables -t nat -A POSTROUTING -o myG -j MASQUERADE
   sudo iptables -A FORWARD -i eth0 -o myG -m state --state RELATED,ESTABLISHED -j ACCEPT
@@ -9,6 +10,7 @@ then
   sudo python3 main.py --unit 0
 elif [ $1 = "mobile" ]
 then
+  sudo ip link delete myG
   sudo iptables -F
   sudo iptables -A INPUT -i myG -m state --state RELATED,ESTABLISHED -j ACCEPT
   sudo iptables -A OUTPUT -o myG -m state --state NEW,RELATED,ESTABLISHED -j ACCEPT
