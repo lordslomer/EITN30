8a9bb5d50f4edf33dc332f11d2923f3335ef3392
SalamSki <slome.lord123@gmail.com>
2024-04-26 10:30:11 +0200

    change back

--- run.sh
+++ run.sh
@@ -2,9 +2,9 @@
 if [ $1 = "base" ]
 then
   sudo iptables -F
-  sudo iptables -t nat -A POSTROUTING -o wlan0 -j MASQUERADE
-  sudo iptables -A FORWARD -i wlan0 -o myG -m state --state RELATED,ESTABLISHED -j ACCEPT
-  sudo iptables -A FORWARD -i myG -o wlan0 -j ACCEPT
+  sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+  sudo iptables -A FORWARD -i eth0 -o myG -m state --state RELATED,ESTABLISHED -j ACCEPT
+  sudo iptables -A FORWARD -i myG -o eth0 -j ACCEPT
   sudo python3 main.py --unit 0
 elif [ $1 = "mobile" ]
 then
