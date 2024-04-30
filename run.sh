b7901b5267a642d38bb3ffc40b7d56c9a473e6a4
SalamSki <slome.lord123@gmail.com>
2024-05-01 00:04:09 +0200

    write code in cpp

--- run.sh
+++ run.sh
@@ -5,8 +5,10 @@ then
   sudo iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
   sudo iptables -A FORWARD -i eth0 -o myG -m state --state RELATED,ESTABLISHED -j ACCEPT
   sudo iptables -A FORWARD -i myG -o eth0 -j ACCEPT
-  sudo python3 main.py --unit 0
+  sudo g++ -std=c++11 -o main main.cpp -lrf24 -ltuntap && sudo ./main 0
+  # sudo python3 main.py --unit 0
 elif [ $1 = "mobile" ]
 then
-  sudo python3 main.py --unit 1
+  sudo g++ -std=c++11 -o main main.cpp -lrf24 -ltuntap && sudo ./main 1
+  # sudo python3 main.py --unit 1
 fi
