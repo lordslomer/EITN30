096000b640279853d7320655e16dee391dbaa2a3
SalamSki <slome.lord123@gmail.com>
2024-05-14 23:53:21 +0200

    show test number (client iperf3)

--- test/iperf3_client.py
+++ test/iperf3_client.py
@@ -28,26 +28,25 @@ def run_test(load):
 # The interval step to take between loads
 speed_step = (MAX_CAPACITY-LOWEST_TEST_SPEED)/((NBR_OF_TESTS*2)/3)
 
-# Build the list of speeds to test 
-loads = [int(LOWEST_TEST_SPEED + i * speed_step) for i in range(NBR_OF_TESTS)]
-
 # Dict that stores the traffic intensity for each test
 archive = {}
 
-for load in loads:
+for i in range(NBR_OF_TESTS):
+    load = int(LOWEST_TEST_SPEED + i * speed_step)
     results = run_test(load)
+
     if results.error:
         print(results.error)
     else:
         rho = results.bps / MAX_CAPACITY
-        print(f'Test completed for offered load {load} bps (ρ = {rho:.2f}):')
+        archive[results.time] = rho
+        print(f'Test {i+1} completed for offered load {load} bps (ρ = {rho:.2f}):')
         print(f'  Took {results.seconds:.2f} seconds started at {results.time}')
         print(f'  Packet loss (%): {results.lost_percent}')
         print(f'  Jitter (ms): {results.jitter_ms:.4f}')
-        print(f'  client bps: {results.bps:.2f}')
+        print(f'  client (Kbps):: {results.bps:.2f}')
         print("")
-        archive[results.time] = rho
-
+        
 # Save the archive dict into a file for plotting
 if len(archive) == NBR_OF_TESTS:
     with open('results/iperf3_client_results.txt', 'w') as client_results: 
