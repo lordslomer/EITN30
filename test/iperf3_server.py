1be25283ba0e54051c2b30a68d75822980b1b335
SalamSki <slome.lord123@gmail.com>
2024-05-14 23:53:03 +0200

    Show test number (server iperf3)

--- test/iperf3_server.py
+++ test/iperf3_server.py
@@ -15,21 +15,20 @@ archive = {}
 
 # Perform and record the result of the next 24 tests
 print("Waiting for the first test...")
-while NBR_OF_TESTS > 0:
+for i in range(NBR_OF_TESTS):
     results = server.run()
     if results.error:
         print(results.error)
     else:
-        NBR_OF_TESTS-=1
-        print('Test completed:')
-        print(f'  Took {results.seconds:.2f} seconds started at {results.time}')
-        print(f'  Packet loss (%): {results.lost_percent}')
-        print(f'  Jitter (ms): {results.jitter_ms:.4f}')
         server_load = results.json['end']['sum_received']['bits_per_second']/1000
-        print(f'  server (Kbps): {server_load:.2f}')
-        print("")
         archive[results.time] = server_load
-
+        print(f'Test {i+1} completed:')
+        print(f'  Took {results.seconds:.2f} seconds started at {results.time}')
+        print(f'  Packet loss (%):  {results.lost_percent}')
+        print(f'  Jitter (ms):      {results.jitter_ms:.4f}')
+        print(f'  server (Kbps):    {server_load:.2f}')
+        print("")
+        
 # Save the archive dict into a file for plotting
 if len(archive) == NBR_OF_TESTS:
     with open('results/iperf3_server_results.txt', 'w') as server_results: 
