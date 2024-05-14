fd03a67188c35d832d4c48abd0be22c9d9f358a4
SalamSki <slome.lord123@gmail.com>
2024-05-14 23:40:35 +0200

    add comments to iperf3 client

--- test/iperf3_client.py
+++ test/iperf3_client.py
@@ -3,12 +3,18 @@ import iperf3
 
 SERVER_ADDR = '10.0.0.1'
 PORT = 12739
+
+# Default iperf3 udp test params
 PACKET_SIZE = 1448
 TEST_DURATION = 10
 PROTOCOL = 'udp'
-MAX_CAPACITY = 382000
-LOWEST_TEST_SPEED = 2000
 
+# Help determine the list of speeds/loads to be tested
+LOWEST_TEST_SPEED = 2000
+NBR_OF_TESTS = 24
+MAX_CAPACITY = 382000
+
+# performs a iperf3 udp test as a client with the options above
 def run_test(load):
     client = iperf3.Client()
     client.server_hostname = SERVER_ADDR
@@ -19,9 +25,13 @@ def run_test(load):
     client.bandwidth = load
     return client.run()
 
-speed_step = (MAX_CAPACITY-LOWEST_TEST_SPEED)/16
-loads = [int(LOWEST_TEST_SPEED + i * speed_step) for i in range(24)]
+# The interval step to take between loads
+speed_step = (MAX_CAPACITY-LOWEST_TEST_SPEED)/((NBR_OF_TESTS*2)/3)
 
+# Build the list of speeds to test 
+loads = [int(LOWEST_TEST_SPEED + i * speed_step) for i in range(NBR_OF_TESTS)]
+
+# Dict that stores the traffic intensity for each test
 archive = {}
 
 for load in loads:
@@ -37,6 +47,8 @@ for load in loads:
         print(f'  client bps: {results.bps:.2f}')
         print("")
         archive[results.time] = rho
-        
-with open('iperf3_client_results.txt', 'w') as client_results: 
-     client_results.write(json.dumps(archive))
+
+# Save the archive dict into a file for plotting
+if len(archive) == NBR_OF_TESTS:
+    with open('results/iperf3_client_results.txt', 'w') as client_results: 
+        client_results.write(json.dumps(archive))
