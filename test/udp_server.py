2398121ee8d556e178fac73afa62cd04b376e583
SalamSki <slome.lord123@gmail.com>
2024-05-15 16:02:55 +0200

    UDP_server done

--- test/udp_server.py
+++ test/udp_server.py
@@ -19,13 +19,22 @@ def time_in_micro():
 def micro_to_sec(micro):
     return micro / 1000000
 
-
+RTTs =[]
 try:
     while True:
         # Receive packets
-        message, client_addr = socket_server.recv(PACKET_SIZE) 
-        timestamp = int.from_bytes(message[:8], 'big')
-        rtt = time_in_micro() - timestamp
+        message, client_addr = socket_server.recvfrom(PACKET_SIZE) 
+        header = message[:8]
+
+        if header == b"E" * 8:
+            avg_rtt = int(sum(RTTs)/len(RTTs))
+            result_payload = avg_rtt.to_bytes(4, 'big')
+            socket_server.sendto(result_payload, client_addr)
+            print(f"Test done with an avg latency: {avg_rtt/1000:.2f} ms\n")
+            RTTs = []
+        else:
+            timestamp = int.from_bytes(header, 'big')
+            RTTs.append(time_in_micro() - timestamp)
 
 except KeyboardInterrupt:
     socket_server.close()
